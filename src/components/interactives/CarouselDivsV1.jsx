a431e9bd38567a644563c6948189d3f5dcf36ccd
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-09-10 14:42:33 -0300

    fix: Todos os prints inseridos

--- src/components/interactives/CarouselDivsV1.jsx
+++ src/components/interactives/CarouselDivsV1.jsx
@@ -3,21 +3,22 @@ import { Carousel } from "primereact/carousel";
 import "../../index.css";
 
 export default function CarouselDivsV1({ children }) {
+  // Configuração de responsividade
   const responsiveOptions = [
     {
       breakpoint: "2500px",
       numVisible: 3,
-      numScroll: 2,
-    }, 
+      numScroll: 1, // 👈 andar de 1 em 1
+    },
     {
       breakpoint: "1199px",
       numVisible: 3,
-      numScroll: 2,
+      numScroll: 1,
     },
     {
       breakpoint: "767px",
       numVisible: 2,
-      numScroll: 2,
+      numScroll: 1,
     },
     {
       breakpoint: "575px",
@@ -26,6 +27,7 @@ export default function CarouselDivsV1({ children }) {
     },
   ];
 
+  // Template de cada item
   const itemTemplate = (item) => {
     return (
       <div className="border-1 rounded text-center py-5 px-3 h-full w-auto">
@@ -35,20 +37,20 @@ export default function CarouselDivsV1({ children }) {
   };
 
   return (
-    <div className="">
+    <div>
       <Carousel
         value={children}
         numVisible={3}
-        numScroll={2}
+        numScroll={1} // 👈 garante que o último não some
         responsiveOptions={responsiveOptions}
         className="text-colorWhite"
         circular
-        autoplayInterval={8000}
+        // autoplayInterval={8000} // habilite se quiser autoplay
         itemTemplate={itemTemplate}
-        showNavigators={false}
+        showNavigators={true} // habilita arrows
         showIndicators={false}
-        prevIcon={<span style={{ color: "white", fontSize: "200%" }}>❮</span>}
-        nextIcon={<span style={{ color: "white", fontSize: "200%" }}>❯</span>}
+        prevIcon={<span style={{ color: "#F49F22", fontSize: "200%" }}>❮</span>}
+        nextIcon={<span style={{ color: "#F49F22", fontSize: "200%" }}>❯</span>}
       />
     </div>
   );
