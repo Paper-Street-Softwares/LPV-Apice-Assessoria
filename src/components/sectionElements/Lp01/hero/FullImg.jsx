2d2dd96407da36367d16491de7106afc55de6bdb
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-11-07 15:22:40 -0300

    fix: Teste

--- src/components/sectionElements/Lp01/hero/FullImg.jsx
+++ src/components/sectionElements/Lp01/hero/FullImg.jsx
@@ -21,7 +21,8 @@ function FullImg({ obs }) {
             alt="Fundo do banner principal"
             fetchpriority="high"
             loading="eager"
-            className="absolute inset-0 w-full h-full object-cover -z-10"
+            className="inset-0 w-full h-full object-cover "
+            //  className="absolute -z-10 inset-0 w-full h-full object-cover "
             decoding="async"
           />
 
